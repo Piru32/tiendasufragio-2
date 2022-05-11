7c42529ee0308fbd4758efb08db81d13f10bd693
juan-zapata-2006 <juanjosezapatamosquera@gmail.com>
2022-05-11 17:23:09 -0500

    esta mierda

---
+++ controlador.js
@@ -0,0 +1,180 @@
+//OBJETOS
+let producto1={
+    nombre:"Pantalon 1",
+    precio:50000,
+    estado: true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-f8214.appspot.com/o/imagen1.jpg?alt=media&token=66650e0a-6e5c-4a21-b685-890d46e30781"
+}
+
+let producto2={
+    nombre:"Pantalon 2",
+    precio:50000,
+    estado: true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-f8214.appspot.com/o/imagen2.jpg?alt=media&token=439711fe-3c0e-4b25-8386-0622a5e28730"
+}
+
+let producto3={
+    nombre:"Camiseta 1",
+    precio:50000,
+    estado: true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-f8214.appspot.com/o/imagen3.jpg?alt=media&token=55d6cb19-efcf-4e8a-9548-a96624c5e45c"
+}
+
+let producto4={
+    nombre:"Camiseta 2",
+    precio:50000,
+    estado: true,
+foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-f8214.appspot.com/o/imagen4.jpg?alt=media&token=03671de3-20ab-4648-8e2b-1956db0fcada"
+}
+
+let producto5={
+    nombre:"Buzo 1",
+    precio:50000,
+    estado: true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-f8214.appspot.com/o/imagen5.jpg?alt=media&token=553d5d69-186a-425a-ad6e-f8d87f2b627a"
+}
+
+let producto6={
+    nombre:"Buzo 2",
+    precio:50000,
+    estado: true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-f8214.appspot.com/o/imagen6.jpg?alt=media&token=f225c9df-cb3d-46e0-975b-c968e4db331e"
+}
+
+let producto7={
+    nombre:"Camiseta 3",
+    precio:50000,
+    estado: true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-f8214.appspot.com/o/imagen8.jpg?alt=media&token=c16f1b7a-2ad4-4c55-a385-74ca6e36ef99"
+}
+
+let producto8={
+    nombre:"Short 1",
+    precio:50000,
+    estado: true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-f8214.appspot.com/o/imagen9.jpg?alt=media&token=7761499a-ac33-4b4f-83ec-45294630fd0a"
+}
+
+let producto9={
+    nombre:"Short 2",
+    precio:50000,
+    estado: true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-f8214.appspot.com/o/imagen10.jpg?alt=media&token=f41e6ed1-db86-48c8-a06f-e8f9ec717192"
+}
+
+let producto10={
+    nombre:"Tenis 1",
+    precio:50000,
+    estado: true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-f8214.appspot.com/o/imagen11.jpg?alt=media&token=df7231ca-7e11-4103-af19-e1eae7b23901"
+}
+
+let producto11={
+    nombre:"Tenis 2",
+    precio:50000,
+    estado: true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-f8214.appspot.com/o/imagen12.jpg?alt=media&token=7a92d085-374a-4995-9bd6-458e979c53e2"
+}
+
+let producto12={
+    nombre:"Tenis 3",
+    precio:50000,
+    estado: true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-f8214.appspot.com/o/imagen13.jpg?alt=media&token=65f92574-c09c-4dab-b3c6-ff8ce8cbc281"
+}
+
+let producto13={
+    nombre:"Tenis 4",
+    precio:50000,
+    estado: true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-f8214.appspot.com/o/imagen14.jpg?alt=media&token=8b1164cb-70c3-4af6-9285-5ab0fbeca09b"
+}
+
+let producto14={
+    nombre:"Medias 1",
+    precio:50000,
+    estado: true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-f8214.appspot.com/o/imagen15.jpg?alt=media&token=85078262-d114-41eb-a2ec-a5906a830d09"
+}
+
+let producto15={
+    nombre:"Gorra 1",
+    precio:50000,
+    estado: true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-f8214.appspot.com/o/imagen16.jpg?alt=media&token=ef7dc10e-24dc-48b6-9668-0f1ff05b2302"
+}
+let producto16={
+    nombre:"Medias 2",
+    precio:50000,
+    estado: true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-f8214.appspot.com/o/imagen17.jpg?alt=media&token=d197c80f-070b-4687-b3df-d5851b7f52b7"
+}
+let producto17={
+    nombre:"Correa 1",
+    precio:50000,
+    estado: true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-f8214.appspot.com/o/imagen18.jpg?alt=media&token=4ddd2cfa-30f9-43dc-857e-46652e1c3299"
+}
+let producto18={
+    nombre:"Cadena 1",
+    precio:50000,
+    estado: true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-f8214.appspot.com/o/imagen19.jpg?alt=media&token=b2d245b8-73f9-454b-944c-71ca59393f1d"
+}
+let producto19={
+    nombre:"Anillos 1",
+    precio:50000,
+    estado: true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-f8214.appspot.com/o/imagen20.jpg?alt=media&token=c1734abd-afa6-476c-9cb9-bc5781ca2c0b"
+}
+let producto20={
+    nombre:"Camiseta 4",
+    precio:50000,
+    estado: true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-f8214.appspot.com/o/imagen7.jpg?alt=media&token=00d2e8fa-eef7-459e-8225-34d486fa559c"
+}
+
+
+//ARREGLO
+let productos=Array(
+    producto1,
+    producto2,
+    producto3,
+    producto4,
+    producto5,
+    producto6,
+    producto7,
+    producto8,
+    producto9,
+    producto10,
+    producto11,
+    producto12,
+    producto13,
+    producto14,
+    producto15,
+    producto16,
+    producto17,
+    producto18,
+    producto19,
+    producto20
+)
+
+let contenedor = document.getElementById("contenedor")
+
+//RECORRO EL ARREGLO
+productos.forEach(function(producto){
+    let foto = document.createElement("img")
+    foto.classList.add("w-100", "img-fluid", "mt-2")
+    foto.src = producto.foto
+    
+    let columna = document.createElement("div")
+    columna.classList.add("col")
+
+    let tarjeta = document.createElement("div")
+    tarjeta.classList.add("card")
+    tarjeta.appendChild(foto)
+    columna.appendChild(tarjeta)
+    contenedor.appendChild(columna)
+
+})
+
